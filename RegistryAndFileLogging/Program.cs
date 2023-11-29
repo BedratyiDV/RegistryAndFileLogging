ce2304b054db3f48c56ff21948e8a56dc5810493
BedratyiDV <bedrik2023@gmail.com>
2023-11-29 16:06:53 +0100

    The First Commit
    29/11/2023

---
+++ RegistryAndFileLogging/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+
+namespace RegistryAndFileLogging
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //New instance of ReadWrightClass class
+            ReadWrightClass readWrightClass = new ReadWrightClass();
+
+            //Path to file containing number of starts of the program
+            string filePath = "C:\\Users\\denis\\source\\Hillel\\Pro\\RegistryAndFileLogging\\number_of_starts.txt";
+            //Path to log file 
+            string logFilePath = "C:\\Users\\denis\\source\\Hillel\\Pro\\RegistryAndFileLogging\\log.txt";
+
+            int startsNumber = readWrightClass.ReadFromFile(filePath);
+
+            //The brench of code execuring while first run of the program
+            if (startsNumber == 0)
+            {
+                RegistryKey regKey = Registry.CurrentUser;
+                var subKey = regKey.CreateSubKey("Registry_And_File_Logging_Application");
+                subKey.SetValue("Start_Number", 1);
+                subKey.SetValue("Start_DateTime", DateTime.Now);
+                subKey.Close();
+            }
+            //Increasing the number of starts of the program   
+            startsNumber++;
+
+            //Writing to the file the new number of starts of the program
+            readWrightClass.WriteToFile(filePath, startsNumber);
+
+            //Appending info regarding the new start of the program into the log file
+            readWrightClass.WriteToLogFile(logFilePath, startsNumber);
+                      
+
+            //Reading of log file content and it's console output
+            readWrightClass.ReadFromLogFile(logFilePath);
+                   
+        }
+    }
+}
+    
---
+++ RegistryAndFileLogging/ReadWrightClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryAndFileLogging
+{
+    //The class contains methods for readind/writing from/into txt file
+   internal class ReadWrightClass
+    {
+        /// <summary>
+        /// Reads number of starts of the program from txt file. Initially the file is filled with 0
+        /// </summary>
+        ///  <param name="filePath"> </param> 
+        public int ReadFromFile(string filePath)
+        {
+            string countString = File.ReadAllText(filePath);
+
+            if (int.TryParse(countString, out int count))
+            {
+                return count;
+            }
+
+            return 0;
+        }
+        /// <summary>
+        /// Writes number of starts of the program into txt file.
+        /// </summary>
+        ///  <param name="filePath"> </param> 
+        ///  <param name="count"> </param>
+        public void WriteToFile(string filePath, int count)
+        {
+            File.WriteAllText(filePath, count.ToString());
+        }
+        /// <summary>
+        /// Reads content of log txt file and output the received content to console.
+        /// </summary>
+        ///  <param name="filePath"> </param> 
+        public void ReadFromLogFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string logRecords = reader.ReadToEnd();
+                Console.WriteLine(logRecords);
+                reader.Close();
+
+            }
+        }
+        /// <summary>
+        /// Writes number of starts of the program into txt log file with appending.
+        /// </summary>
+        ///  <param name="filePath"> </param> 
+        ///  <param name="count"> </param>
+        public void WriteToLogFile(string filePath, int count)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now} - Program start {count}");
+                writer.Close();
+
+            }
+        }
+        
+    }
+}
